bce41c0ff7acb71b67c42789e8b9fa272f787832
TheBetioPlane <TheBetioPlane@users.noreply.github.com>
2020-06-07 15:33:36 -0400

    changed texture filter from Nearest to Linear
    Nearest kind of looked bad and I don't know why it was set to that in
    the first place

--- ReplayViewer/Texture2D.cs
+++ ReplayViewer/Texture2D.cs
@@ -21,8 +21,8 @@ namespace ReplayViewer
                 this.Height = bitmap.Height;
                 this.textureId = GL.GenTexture();
                 GL.BindTexture(TextureTarget.Texture2D, this.textureId);
-                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
-                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
                 BitmapData data = bitmap.LockBits(new Rectangle(0, 0, this.Width, this.Height), ImageLockMode.ReadOnly,
                     System.Drawing.Imaging.PixelFormat.Format32bppArgb);
                 GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
